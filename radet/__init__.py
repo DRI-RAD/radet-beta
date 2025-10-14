99a08c645b80780d3fa93b55eb37ad50b379bbe2
Bruno Comini <78499248+bcomini@users.noreply.github.com>
2025-10-14 15:49:12 -0300

    Added initial files
    Added files for running a single image.

---
+++ radet/__init__.py
@@ -0,0 +1,10 @@
+from .image import Image
+from .collection import Collection
+from . import interpolate as interpolate
+
+MODEL_NAME = 'RADET'
+
+# from importlib import metadata
+# # __version__ = metadata.version(__package__ or __name__)
+# __version__ = metadata.version(__package__.replace('.', '-') or __name__.replace('.', '-'))
+# # __version__ = metadata.version('openet-geesebal')
